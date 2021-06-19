3a9401346de2da880a7cda1694d0c3bcc7c3ef9f
Harremking0 <chakibou1@hotmail.com>
2021-06-19 01:12:53 +0100

    created Navbar component

---
+++ src/components/Navbar.js
@@ -0,0 +1,23 @@
+import React from 'react'
+
+export const Navbar = () => {
+    return (
+        <div className='navbar'>
+            <ul>
+                <li className="nav-element">
+
+                </li>
+
+                <li className="nav-element">
+
+                </li>
+                
+                <li className="nav-element">
+
+                </li>
+                
+                
+            </ul>
+        </div>
+    )
+}
