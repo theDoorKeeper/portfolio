32eb01b82e799db6e9d6d8999d311b00df2d2a72
Harremking0 <chakibou1@hotmail.com>
2021-06-19 02:28:31 +0100

    added route for each Component

--- src/App.js
+++ src/App.js
@@ -1,19 +1,33 @@
 import './App.css';
-
+import About from './components/About';
+import { Navbar } from './components/Navbar';
+import { Skills } from './components/Skills';
+import { Projects } from './components/Projects'
 import {
   BrowserRouter as Router,
   Switch,
   Route,
-  Link
 } from "react-router-dom";
-import { Navbar } from './components/Navbar';
+
 function App() {
   return (
     <Router> 
     <div className="App">
       <Navbar/>
       <Switch>
-        
+
+        <Route exact path='/'>
+        <About/>
+        </Route>
+
+          <Route exact path='/Skills'>
+          <Skills/>
+          </Route>
+
+        <Route exact path='/Projects'>
+          <Projects/>
+          </Route>
+
       </Switch>
     </div>
     </Router>
