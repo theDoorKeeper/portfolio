a4b9d2e8b1dbd7befd5bc9d3065d039bb3b74b2a
Harremking0 <chakibou1@hotmail.com>
2021-06-19 02:36:45 +0100

    imported and set up navlink

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,20 +1,27 @@
 import React from 'react'
+import { NavLink } from 'react-router-dom';
 
 export const Navbar = () => {
     return (
         <div className='navbar'>
             <ul>
+                <NavLink to="/">
                 <li className="nav-element">
 
                 </li>
+                 </NavLink>
 
+                 <NavLink to="/">
                 <li className="nav-element">
 
                 </li>
-                
+                </NavLink>
+
+                <NavLink to="/">
                 <li className="nav-element">
 
                 </li>
+                </NavLink>
                 
                 
             </ul>
