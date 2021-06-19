935cbf2634deafb590c91f9026f4c272a7a66e21
Harremking0 <chakibou1@hotmail.com>
2021-06-19 01:33:24 +0100

    added browser router dom and react motion libraries

--- src/App.js
+++ src/App.js
@@ -1,9 +1,22 @@
 import './App.css';
 
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
+import { Navbar } from './components/Navbar';
 function App() {
   return (
+    <Router> 
     <div className="App">
+      <Navbar/>
+      <Switch>
+        
+      </Switch>
     </div>
+    </Router>
   );
 }
 
