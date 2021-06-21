f344e2ee506c798cd9befea896d1a8377b183dac
Harremking0 <chakibou1@hotmail.com>
2021-06-21 21:42:52 +0100

    added description and logos to each project

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -7,6 +7,12 @@ import calculator from '../assets/Projects-images/calculator.png';
 import shop from '../assets/Projects-images/shop.png';
 import tictactoe from '../assets/Projects-images/tictactoe.png';
 import todo from '../assets/Projects-images/todo.png';
+import css from '../assets/css.svg';
+import html from '../assets/html.svg';
+import react from '../assets/react.svg';
+import js from '../assets/JS.svg';
+import github from '../assets/github.svg';
+
 export const Projects = () => {
     return (
         <div className="project-container">
@@ -14,34 +20,48 @@ export const Projects = () => {
              <header className="title">Project : Shopping cart</header>
              <img src={shop}></img>
              <div className="project-description">
-             Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime mollitia,
-            molestiae quas vel sint commodi repudiandae consequuntur voluptatum laborum
-            numquam blanditiis harum quisquam eius sed odit fugiat iusto fuga praesentium
-            optio, eaque rerum! Provident similique accusantium nemo autem. Veritatis
-            obcaecati tenetur iure eius earum ut molestias architecto voluptate aliquam
-            nihil, eveniet aliquid culpa officia aut! Impedit sit sunt quaerat, odit,
-            tenetur error, harum nesciunt ipsum debitis quas aliquid. Reprehenderit,
-            quia. Quo neque error repudiandae fuga? Ipsa laudantium molestias eos 
-            sapiente officiis modi at sunt excepturi expedita sint? Sed quibusdam
-            recusandae alias error harum maxime adipisci amet laborum.
+            Technologies used : <img className="technology" src={react}/>   <img className="technology" src={html}/>   <img className="technology" src={css}/> .<br/>
+            Desc : shopping cart made with react an react router dom , used framer motion for the animations , it has a navabar with the number of items purchased next to 
+            the cart icon  , the catalogue is fetched from the Fortnine API  , you can increment and decrement the number of items bought from the catalogue and the cart ,
+            i used lifted states to send the data between components . (i was going to use USeContext , but decided it was'nt needed in this project).
              </div>
+
+             <div className="project-links">
+
+             <a href="https://github.com/theDoorKeeper/shopping-cart-react" target="_blank">
+	            <img src={github} />
+             </a>
+
+             <a href="https://thedoorkeeper.github.io/shopping-cart-react/" target="_blank">
+	            <button className="live-button">Live code</button>
+             </a>
+
+             </div>
+             
             </div>
 
             <div className="project">
             <header className="title">Project : Memory card game</header>
             <img src={memoryGame}></img>
             <div className="project-description">
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime mollitia,
-            molestiae quas vel sint commodi repudiandae consequuntur voluptatum laborum
-            numquam blanditiis harum quisquam eius sed odit fugiat iusto fuga praesentium
-            optio, eaque rerum! Provident similique accusantium nemo autem. Veritatis
-            obcaecati tenetur iure eius earum ut molestias architecto voluptate aliquam
-            nihil, eveniet aliquid culpa officia aut! Impedit sit sunt quaerat, odit,
-            tenetur error, harum nesciunt ipsum debitis quas aliquid. Reprehenderit,
-            quia. Quo neque error repudiandae fuga? Ipsa laudantium molestias eos 
-            sapiente officiis modi at sunt excepturi expedita sint? Sed quibusdam
-            recusandae alias error harum maxime adipisci amet laborum.
+            Technologies used : <img className="technology" src={react}/>   <img className="technology" src={html}/>   <img className="technology" src={css}/>. <br/>
+            Desc : this is my second react project , a simple memory card game , i imported the data from an array i made manually , the cards are displayed randomly
+            from said array , useEffect is used to render the inital predetrmined card set on mount , and then to change it depending on the array changes , it also
+            includes a score board that displays your current and highscore .
             </div>
+
+            <div className="project-links">
+
+             <a href="https://github.com/theDoorKeeper/shopping-cart-react" target="_blank">
+	            <img src={github} />
+             </a>
+
+             <a href="https://thedoorkeeper.github.io/shopping-cart-react/" target="_blank">
+	            <button className="live-button">Live code</button>
+             </a>
+
+             </div>
+
             </div>
 
 
@@ -49,68 +69,93 @@ export const Projects = () => {
             <header className="title">Project : CV-maker</header>
             <img src={cv}></img>
             <div className="project-description">
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime mollitia,
-            molestiae quas vel sint commodi repudiandae consequuntur voluptatum laborum
-            numquam blanditiis harum quisquam eius sed odit fugiat iusto fuga praesentium
-            optio, eaque rerum! Provident similique accusantium nemo autem. Veritatis
-            obcaecati tenetur iure eius earum ut molestias architecto voluptate aliquam
-            nihil, eveniet aliquid culpa officia aut! Impedit sit sunt quaerat, odit,
-            tenetur error, harum nesciunt ipsum debitis quas aliquid. Reprehenderit,
-            quia. Quo neque error repudiandae fuga? Ipsa laudantium molestias eos 
-            sapiente officiis modi at sunt excepturi expedita sint? Sed quibusdam
-            recusandae alias error harum maxime adipisci amet laborum.
+            Technologies used : <img className="technology" src={react}/>   <img className="technology" src={html}/>   <img className="technology" src={css}/>. <br/>
+            Desc : this is my first react project , it was initialy made with class components then refractored using Functional components and useState , it was challeging
+            confusing at times , the app consist of two components  , one that accepts input and another one to display it , changing the components was somewhat tricky because
+            of the differences between useState and setState , i learned alot during this process .
             </div>
+
+            <div className="project-links">
+
+             <a href="https://github.com/theDoorKeeper/shopping-cart-react" target="_blank">
+	            <img src={github} />
+             </a>
+
+             <a href="https://thedoorkeeper.github.io/shopping-cart-react/" target="_blank">
+	            <button className="live-button">Live code</button>
+             </a>
+
+             </div>
+
             </div>
 
             <div className="project">
             <header className="title">Project : weather App</header>
             <img src={weather}></img>
             <div className="project-description">
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime mollitia,
-            molestiae quas vel sint commodi repudiandae consequuntur voluptatum laborum
-            numquam blanditiis harum quisquam eius sed odit fugiat iusto fuga praesentium
-            optio, eaque rerum! Provident similique accusantium nemo autem. Veritatis
-            obcaecati tenetur iure eius earum ut molestias architecto voluptate aliquam
-            nihil, eveniet aliquid culpa officia aut! Impedit sit sunt quaerat, odit,
-            tenetur error, harum nesciunt ipsum debitis quas aliquid. Reprehenderit,
-            quia. Quo neque error repudiandae fuga? Ipsa laudantium molestias eos 
-            sapiente officiis modi at sunt excepturi expedita sint? Sed quibusdam
-            recusandae alias error harum maxime adipisci amet laborum.
+            Technologies used : <img className="technology" src={js}/>   <img className="technology" src={html}/>   <img className="technology" src={css}/>. <br/>
+            Desc: Simple JS weather App , used async await syntax to fetch data from the OpenWeather API (i understand that pushing an API key is dangerous and should'nt be done ,
+            but this is for showcase and its a free key )
             </div>
+
+            <div className="project-links">
+
+             <a href="https://github.com/theDoorKeeper/shopping-cart-react" target="_blank">
+	            <img src={github} />
+             </a>
+
+             <a href="https://thedoorkeeper.github.io/shopping-cart-react/" target="_blank">
+	            <button className="live-button">Live code</button>
+             </a>
+
+             </div>
+
             </div>
 
             <div className="project">
             <header className="title">Project : Todo app</header>
             <img src={todo}></img>
             <div className="project-description">
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime mollitia,
-            molestiae quas vel sint commodi repudiandae consequuntur voluptatum laborum
-            numquam blanditiis harum quisquam eius sed odit fugiat iusto fuga praesentium
-            optio, eaque rerum! Provident similique accusantium nemo autem. Veritatis
-            obcaecati tenetur iure eius earum ut molestias architecto voluptate aliquam
-            nihil, eveniet aliquid culpa officia aut! Impedit sit sunt quaerat, odit,
-            tenetur error, harum nesciunt ipsum debitis quas aliquid. Reprehenderit,
-            quia. Quo neque error repudiandae fuga? Ipsa laudantium molestias eos 
-            sapiente officiis modi at sunt excepturi expedita sint? Sed quibusdam
-            recusandae alias error harum maxime adipisci amet laborum.
+            Technologies used : <img className="technology" src={js}/>   <img className="technology" src={html}/>   <img className="technology" src={css}/>. <br/>
+            Desc: Todo list app , you can create different projects and store todos inside them , with a title a desc and a date , each project can be edited and deleted 
+            aswell as each todo , you can check the todo if done , each project is generated dinamically , and the code is respecting OOP principles with each part
+            of a different  logic in its own module , factory functions have been used for the todos and the projects .
             </div>
+
+            <div className="project-links">
+
+             <a href="https://github.com/theDoorKeeper/shopping-cart-react" target="_blank">
+	            <img src={github} />
+             </a>
+
+             <a href="https://thedoorkeeper.github.io/shopping-cart-react/" target="_blank">
+	            <button className="live-button">Live code</button>
+             </a>
+
+             </div>
+
             </div>
 
             <div className="project">
             <header className="title">Project : Tictactoe</header>
             <img src={tictactoe}></img>
             <div className="project-description">
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime mollitia,
-            molestiae quas vel sint commodi repudiandae consequuntur voluptatum laborum
-            numquam blanditiis harum quisquam eius sed odit fugiat iusto fuga praesentium
-            optio, eaque rerum! Provident similique accusantium nemo autem. Veritatis
-            obcaecati tenetur iure eius earum ut molestias architecto voluptate aliquam
-            nihil, eveniet aliquid culpa officia aut! Impedit sit sunt quaerat, odit,
-            tenetur error, harum nesciunt ipsum debitis quas aliquid. Reprehenderit,
-            quia. Quo neque error repudiandae fuga? Ipsa laudantium molestias eos 
-            sapiente officiis modi at sunt excepturi expedita sint? Sed quibusdam
-            recusandae alias error harum maxime adipisci amet laborum.
+            Technologies used : <img className="technology" src={js}/>   <img className="technology" src={html}/>   <img className="technology" src={css}/>. <br/>
+            Desc: simple tic tac toe game , with the computer making random moves , this project was great for practicing the module patter and factory functions !
             </div>
+
+            <div className="project-links">
+
+             <a href="https://github.com/theDoorKeeper/shopping-cart-react" target="_blank">
+	            <img src={github} />
+             </a>
+
+             <a href="https://thedoorkeeper.github.io/shopping-cart-react/" target="_blank">
+	            <button className="live-button">Live code</button>
+             </a>
+
+             </div>
+
             </div>
 
 
@@ -118,17 +163,22 @@ export const Projects = () => {
             <header className="title">Project : Calculator</header>
             <img src={calculator}></img>
             <div className="project-description">
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime mollitia,
-            molestiae quas vel sint commodi repudiandae consequuntur voluptatum laborum
-            numquam blanditiis harum quisquam eius sed odit fugiat iusto fuga praesentium
-            optio, eaque rerum! Provident similique accusantium nemo autem. Veritatis
-            obcaecati tenetur iure eius earum ut molestias architecto voluptate aliquam
-            nihil, eveniet aliquid culpa officia aut! Impedit sit sunt quaerat, odit,
-            tenetur error, harum nesciunt ipsum debitis quas aliquid. Reprehenderit,
-            quia. Quo neque error repudiandae fuga? Ipsa laudantium molestias eos 
-            sapiente officiis modi at sunt excepturi expedita sint? Sed quibusdam
-            recusandae alias error harum maxime adipisci amet laborum.
+            Technologies used : <img className="technology" src={js}/>   <img className="technology" src={html}/>   <img className="technology" src={css}/>. <br/>
+            Desc : One of my first JS projects , a simple JS calculator with sounds on click  ! i manly put this project on to show my progress since ! 
             </div>
+
+            <div className="project-links">
+
+             <a href="https://github.com/theDoorKeeper/shopping-cart-react" target="_blank">
+	            <img src={github} />
+             </a>
+
+             <a href="https://thedoorkeeper.github.io/shopping-cart-react/" target="_blank">
+	            <button className="live-button">Live code</button>
+             </a>
+
+             </div>
+
             </div>
             
         </div>
