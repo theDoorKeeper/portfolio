58cf598c37e1e690f90a14c2734a00848a0549fb
Harremking0 <chakibou1@hotmail.com>
2021-06-21 01:57:51 +0100

    added and imported logos for technologies

--- src/components/Skills.js
+++ src/components/Skills.js
@@ -1,9 +1,14 @@
 import React from 'react'
+import css from '../assets/css.svg';
+import html from '../assets/html.svg';
+import react from '../assets/react.svg';
+import git from '../assets/git.svg';
+import js from '../assets/JS.svg';
 
 export const Skills = () => {
     return (
-        <div>
-            
+        <div className="skils-wrapper">
+    
         </div>
     )
 }
