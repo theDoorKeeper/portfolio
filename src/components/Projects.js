6b329dfbee785516dc3cabca9aa7cfd9e55b1d56
Harremking0 <chakibou1@hotmail.com>
2021-06-21 02:58:25 +0100

    added mockup projects and styled them

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,13 +1,33 @@
 import React from 'react'
-
+import './Project.css'
 export const Projects = () => {
     return (
         <div className="project-container">
-            <div className="project"></div>
-            <div className="project"></div>
-            <div className="project"></div>
-            <div className="project"></div>
-            <div className="project"></div>
+            <div className="project">
+             <header className="title">Project : blablabla</header>
+             <img src=""></img>
+             <div className="project-description"></div>
+            </div>
+            <div className="project">
+            <header className="title">Project : blablabla</header>
+            <img src=""></img>
+            <div className="project-description"></div>
+            </div>
+            <div className="project">
+            <header className="title">Project : blablabla</header>
+            <img src=""></img>
+            <div className="project-description"></div>
+            </div>
+            <div className="project">
+            <header className="title">Project : blablabla</header>
+            <img src=""></img>
+            <div className="project-description"></div>
+            </div>
+            <div className="project">
+            <header className="title">Project : blablabla</header>
+            <img src=""></img>
+            <div className="project-description"></div>
+            </div>
             
         </div>
     )
