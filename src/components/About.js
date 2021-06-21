9fd32460e2dc4a7d22690b377231fdb61d949cce
Harremking0 <chakibou1@hotmail.com>
2021-06-21 06:00:46 +0100

    imported and added the images to project component

--- src/components/About.js
+++ src/components/About.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import './About.css';
-
 export const About = () => {
     return (
         <div className="about-main">
--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,31 +1,48 @@
-import React from 'react'
-import './Project.css'
+import React from 'react';
+import './Project.css';
+import memoryGame from '../assets/Projects-images/memory-game.png';
+import weather from '../assets/Projects-images/weather.png';
+import cv from '../assets/Projects-images/cv.png';
+import calculator from '../assets/Projects-images/calculator.png';
+import shop from '../assets/Projects-images/shop.png';
+import tictactoe from '../assets/Projects-images/tictactoe.png';
+import todo from '../assets/Projects-images/todo.png';
 export const Projects = () => {
     return (
         <div className="project-container">
             <div className="project">
              <header className="title">Project : blablabla</header>
-             <img src=""></img>
+             <img src={shop}></img>
              <div className="project-description"></div>
             </div>
             <div className="project">
             <header className="title">Project : blablabla</header>
-            <img src=""></img>
+            <img src={memoryGame}></img>
             <div className="project-description"></div>
             </div>
             <div className="project">
             <header className="title">Project : blablabla</header>
-            <img src=""></img>
+            <img src={cv}></img>
             <div className="project-description"></div>
             </div>
             <div className="project">
             <header className="title">Project : blablabla</header>
-            <img src=""></img>
+            <img src={weather}></img>
             <div className="project-description"></div>
             </div>
             <div className="project">
             <header className="title">Project : blablabla</header>
-            <img src=""></img>
+            <img src={todo}></img>
+            <div className="project-description"></div>
+            </div>
+            <div className="project">
+            <header className="title">Project : blablabla</header>
+            <img src={tictactoe}></img>
+            <div className="project-description"></div>
+            </div>
+            <div className="project">
+            <header className="title">Project : blablabla</header>
+            <img src={calculator}></img>
             <div className="project-description"></div>
             </div>
             
