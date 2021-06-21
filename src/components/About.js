4d6dced704049c191a1bbc343b09dfc548590b63
Harremking0 <chakibou1@hotmail.com>
2021-06-21 01:03:26 +0100

    added a title and description for the about component

--- src/components/About.js
+++ src/components/About.js
@@ -2,7 +2,17 @@ import React from 'react'
 
 export const About = () => {
     return (
-        <div>
+        <div className="about-main">
+            <div className="title">
+                <h1> Chakib Boumaraf </h1>
+                <h4>Front end develloper</h4>
+                </div>    
+                <div className="description">
+            <p>Hi , I'm Chakib  a self taught front end develloper , currenctly spesialized in React and still learning , planning to go full stack using NodeJS ,
+                I discovered a passion in programming , and decided to drop out to continue on this road , i love learning new concepts ,applying them , solving problems,
+                my grit is what got me from not knowing what html was to building a few projects !
+                     </p>
+                </div>
             
         </div>
     )
