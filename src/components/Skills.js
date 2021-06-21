1e2a765cbe5b8d552ae498d6594cd8f10af9bae4
Harremking0 <chakibou1@hotmail.com>
2021-06-21 02:03:10 +0100

    displayed the logos

--- src/components/Skills.js
+++ src/components/Skills.js
@@ -8,7 +8,11 @@ import js from '../assets/JS.svg';
 export const Skills = () => {
     return (
         <div className="skils-wrapper">
-    
+            <img src={css} alt="css logo" className="skil-logo"/>
+            <img src={html} alt="html logo" className="skil-logo"/>
+            <img src={react} alt="react logo" className="skil-logo"/>
+            <img src={git} alt="git logo" className="skil-logo"/>
+            <img src={js} alt="js logo" className="skil-logo"/>
         </div>
     )
 }
